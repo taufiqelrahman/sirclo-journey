4fa14644d8bc5e91f965fbf6ede222654be48958
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2017-11-28 07:13:35 +0700

    created and passed addProduct test

--- shopping-cart/Cart.js
+++ shopping-cart/Cart.js
@@ -7,6 +7,19 @@ class Cart {
   getCart() {
     return this._cart
   }
+  addProduct(code, quantity) {
+    let exists = this._cart.find(item => item.product.getName() == code)
+    if (exists) {
+      let oldQuantity = this._cart.find(item => item.product.getName() == code).quantity
+      this._cart.find(item => item.product.getName() == code).quantity = oldQuantity + quantity
+    }
+    else {
+      this._cart.push({
+        product: new Product(code),
+        quantity: quantity
+      })
+    } 
+  }
 }
 
 module.exports = Cart
--- shopping-cart/test/Cart.js
+++ shopping-cart/test/Cart.js
@@ -8,5 +8,18 @@ describe('Cart', function() {
       assert.equal(cart.getCart().length, 0)
     })
   })
+  describe('Add product', function() {
+    it('can add product', function() {
+      const cart = new Cart()
+      cart.addProduct('red',2)
+      assert.equal(cart.getCart().length,1)
+    })
+    it('can add the same product', function() {
+      const cart = new Cart()
+      cart.addProduct('red',2)
+      cart.addProduct('red',3)
+      assert.equal(cart.getCart().length,1)
+    })
+  })
 
 })
