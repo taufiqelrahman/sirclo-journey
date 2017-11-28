3dc196729ee88e4e1170ca545ba8f8e3e0255e6b
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2017-11-28 09:30:58 +0700

    init climate-app

---
+++ climate-app/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'index',
+  entry: __dirname + '/src/index.js',
+	output: {
+		filename: 'index.js'
+	},
+  devServer: {
+    contentBase: __dirname + "/src/",
+    port: 9010
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}
